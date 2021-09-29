56f62e7f7dc9b5ea142091d8a2d3ad0f29cef344
ArdXxX <45080754+ArdXxX@users.noreply.github.com>
2021-09-29 13:00:54 +0200

    Init
    Anti AFK for New World

---
+++ main.py
@@ -0,0 +1,35 @@
+import pyautogui
+from win32gui import GetWindowText, GetForegroundWindow
+import win32gui
+import time
+import random
+
+windowName = 'New World'
+hwnd = win32gui.FindWindowEx(0,0,0, windowName)
+keys = ['a', 'd', 'w', 's', 'space']
+
+print('Initializing New World -ANTI AFK-')
+
+def main():
+    while True:
+        if win32gui.FindWindow(None, windowName): 
+            currentWindow = GetWindowText(GetForegroundWindow())
+            if(currentWindow == windowName):
+                key = random.randint(0, 4)
+                pyautogui.press(keys[key])
+                print('NW Anti AFK, sending key : ' + keys[key])    
+            else:
+                win32gui.SetForegroundWindow(hwnd)
+                print('Bringing NW window to front')
+                time.sleep(2)
+                continue
+                    
+            r = random.randint(1, 10)
+            print('Waiting ' + str(r) + ' seconds to repeat')
+            time.sleep(r)
+        else:
+            print('New World not found, terminating')
+            break
+    
+if __name__ == "__main__":
+    main()
